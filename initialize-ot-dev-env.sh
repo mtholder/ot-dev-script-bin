5bf6be8573e6a8f237d9f11390a73f3755ffd38c
Mark T. Holder <mtholder@gmail.com>
2014-02-21 09:45:06 -0600

    some bootstrapping notes

--- initialize-ot-dev-env.sh
+++ initialize-ot-dev-env.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-OPEN_TREE_ROOT="$(dirname \"$PWD\")"
+OPEN_TREE_ROOT="$(dirname $PWD)"
 echo "export OPEN_TREE_ROOT=\"$OPEN_TREE_ROOT\""
+cd $OPEN_TREE_ROOT
+virtualenv env >/dev/null 2>&1 || echo "install of virtualenv to env failed!"
+echo 'source "${OPEN_TREE_ROOT}/env/bin/activate"'
+echo 'export PATH="${PATH}:${OPEN_TREE_ROOT}/bin"'
+
