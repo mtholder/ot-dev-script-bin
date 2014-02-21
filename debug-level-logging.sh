bfbb774780032d2debb4099ca5017fb103857ff5
Mark T. Holder <mtholder@gmail.com>
2014-02-21 13:57:11 -0600

    bug fix

--- debug-level-logging.sh
+++ debug-level-logging.sh
@@ -5,5 +5,5 @@ export PEYOTL_LOGGING_FORMAT=simple
 export OT_API_LOGGING_LEVEL=debug
 export OT_API_LOGGING_FORMAT=simple
 mv "${OPEN_TREE_ROOT}/opentree/curator/private/config" "${OPEN_TREE_ROOT}/opentree/curator/private/config.BAK"
-mv "${OPEN_TREE_ROOT}/opentree/curator/private/config.default_logging" "${OPEN_TREE_ROOT}/opentree/curator/private/config"
+mv "${OPEN_TREE_ROOT}/opentree/curator/private/config.debug_logging" "${OPEN_TREE_ROOT}/opentree/curator/private/config"
 set +x
