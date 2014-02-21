c98fc96c1d5900d442223d44f04aba41c584cfca
Mark T. Holder <mtholder@gmail.com>
2014-02-21 13:37:01 -0600

    switch to normal logging

--- initialize-ot-dev-env.sh
+++ initialize-ot-dev-env.sh
@@ -5,6 +5,7 @@ cd $OPEN_TREE_ROOT
 virtualenv env >/dev/null 2>&1 || echo "install of virtualenv to env failed!"
 echo 'source "${OPEN_TREE_ROOT}/env/bin/activate"'
 echo 'alias debug-logging="source ${OPEN_TREE_ROOT}/bin/debug-level-logging.sh && restart-web2py"'
+echo 'alias normal-logging="source ${OPEN_TREE_ROOT}/bin/normal-level-logging.sh && restart-web2py"'
 echo 'export PATH="${PATH}:${OPEN_TREE_ROOT}/bin"'
 echo 'export PEYOTL_ROOT="${OPEN_TREE_ROOT}/peyotl"'
 echo 'export API_ROOT="${OPEN_TREE_ROOT}/api.opentreeoflife.org"'
---
+++ normal-level-logging.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x
+export PEYOTL_LOGGING_LEVEL=warn
+export PEYOTL_LOGGING_FORMAT=simple
+export OT_API_LOGGING_LEVEL=warn
+export OT_API_LOGGING_FORMAT=simple
+mv "${OPEN_TREE_ROOT}/opentree/curator/private/config" "${OPEN_TREE_ROOT}/opentree/curator/private/config.BAK"
+mv "${OPEN_TREE_ROOT}/opentree/curator/private/config.normal_logging" "${OPEN_TREE_ROOT}/opentree/curator/private/config"
+set +x
